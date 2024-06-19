a7754dea71592aa9808460bfbade043471dcaa8d
Velessinn <jacek.daniluk@strazpozarna.org.pl>
2024-06-19 20:57:20 +0200

    kontekst szablonow

--- intro/hello/urls.py
+++ intro/hello/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
 
-from hello.views import hello_view
+from hello import views
 
 urlpatterns = [
-    path('main/', hello_view),
+    #view intro
+    path('', views.hello_view),
+
+    #templates intro
+    path('hi/', views.hi_view),
+    path('hi2/', views.hi2_view),
+
+    # user input
+    path('byk/', views.byk_view),
+    path('<str:name>/', views.name_view),
+    path('<str:name>/2/', views.name_view2),
 
 ]
--- intro/hello/views.py
+++ intro/hello/views.py
@@ -3,3 +3,42 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def hello_view(request):
     return HttpResponse("Witaj świecie!")
+
+def hi_view(request):
+    return HttpResponse(
+        """
+        <!DOCTYPE html>
+        <html>
+            <head>
+            </head>
+            <body>
+                <h2>Hello, Wolrd!</h2>
+            </body>
+        </html>
+        """)
+
+# szablon
+def hi2_view(request):
+    return render(request, 'hello.html')
+
+def byk_view(request):
+    return HttpResponse("Elo byku malinowy!")
+
+# Podatność XSS - przykład
+def name_view(request,name ):
+    #importy powinny być zawsze na samej górze
+    from django.utils.html import escape
+
+
+    # always remember to escape your output
+    print(name)
+    escaped_name = escape(name)
+    return HttpResponse(f"Witaj, {escaped_name}!")
+
+# kontekst szablonu
+def name_view2(request, name):
+    return render(
+        request,
+        'hello2.html',
+        {"x": name}
+    )
