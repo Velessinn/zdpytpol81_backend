38e581e9aba98b1d5e220f52bb70621a4b4a0391
Velessinn <jacek.daniluk@strazpozarna.org.pl>
2024-07-01 21:06:51 +0200

    dtl - inheritance

---
+++ intro/inheritance/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+def first_view(request):
+    return render(
+        request,
+        "first.html",
+
+    )
+
+def second_view(request):
+    return render(
+        request,
+        "second.html"
+
+    )
+
+def third_view(request):
+    return render(
+        request,
+        "third.html"
+
+    )
