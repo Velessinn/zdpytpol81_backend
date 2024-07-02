9557569f79790b9056c1dbfb3204a9fb75d8f6af
Velessinn <jacek.daniluk@strazpozarna.org.pl>
2024-07-02 19:09:20 +0200

    dtl - links part 2

--- link_app/urls.py
+++ link_app/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from link_app import views
 
+app_name = 'link_app'
+
 urlpatterns = [
-    path('first/', views.first_view),
-    path('second/', views.second_view),
+    path('first/', views.first_view, name='first'),
+    path('drugi/', views.second_view, name='second'),
+    path('third/<str:param>/', views.third_view, name='third'),
 
 ]
--- link_app/views.py
+++ link_app/views.py
@@ -4,12 +4,19 @@ from django.shortcuts import render
 
 def first_view(request):
     return render(
-            request,
-            'link_app/first.html'
-            )
+        request,
+        'link_app/first.html'
+        )
 
 def second_view(request):
     return render(
-            request,
-            'link_app/second.html'
-            )
+        request,
+        'link_app/second.html'
+        )
+
+def third_view(request, param):
+    return render(
+        request,
+        'link_app/third.html',
+        {'param': param}
+        )
