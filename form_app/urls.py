33ad301ffb0c54b77498a09b49035c1ccda36d04
Velessinn <jacek.daniluk@strazpozarna.org.pl>
2024-07-03 19:39:16 +0200

    formularze v2

--- form_app/urls.py
+++ form_app/urls.py
@@ -5,4 +5,6 @@ from form_app import views
 app_name ='form_app'
 urlpatterns = [
     path('hello-get/', views.hello_get, name='hello_get'),
+    path('hello-post/', views.hello_post, name='hello_post'),
+    path('show/', views.show, name='show'),
 ]
