a86cbb4b98d641a71bb8ca4a436d6cd38beca98d
Velessinn <jacek.daniluk@strazpozarna.org.pl>
2024-07-10 18:28:32 +0200

    orm relacje 1 to 1, 1 to many, many to many

--- orm_app/models.py
+++ orm_app/models.py
@@ -3,3 +3,42 @@ from django.db import models
 
 class Task(models.Model):
     name = models.CharField(max_length=255)
+
+
+# 1 to 1
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+    capital = models.OneToOneField('Capital', on_delete=models.CASCADE)
+
+class Capital(models.Model):
+    name = models.CharField(max_length=255)
+
+
+# 1 to many
+class Language(models.Model):
+    name = models.CharField(max_length=64)
+    def __str__(self):
+        return self.name
+
+class Framework(models.Model):
+    name = models.CharField(max_length=255)
+    language = models.ForeignKey('Language', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+# many to many
+
+class Actor(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    actors = models.ManyToManyField('Actor')
+
+    def __str__(self):
+        return self.title
+
